52e1111a4731d8fdf7204365ca4c290ec802903f
VernalR <josegomezs1003@gmail.com>
2025-11-26 20:16:37 -0400

    mSalida added

---
+++ src/Cl_mSalida.ts
@@ -0,0 +1,41 @@
+import Cl_mTransaccion from "./Cl_mTransaccion.js";
+
+export default class Cl_mSalida extends Cl_mTransaccion{
+    private _tipoTransaccion: number = 0;
+    constructor({
+        descripcion,
+        monto,
+        referencia,
+        categoria,
+        fecha,
+        tipoTransaccion = 0,
+    }: {
+        descripcion: string,
+        monto: number,
+        referencia: string,
+        categoria: string,
+        fecha: number,
+        tipoTransaccion: number,
+    }){
+        super({descripcion, monto, referencia, categoria, fecha});
+        this.tipoTransaccion = tipoTransaccion;
+    }
+    // setter y getter
+    public set tipoTransaccion(t: number) {
+        this._tipoTransaccion = t;
+    }
+    public get tipoTransaccion(): number {
+        return this._tipoTransaccion;
+    }
+    //metodo de validacion
+    
+    //metodos
+
+    toJSON(){
+        return {
+            ...super.toJSON(),
+            tipoTransaccion: this.tipoTransaccion,
+        }
+    }
+
+}
