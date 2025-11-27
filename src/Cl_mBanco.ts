76332b713aafd242b08e44d5762c5056cea97ab6
Gabriel20064 <gabrielhernandez20064@gmail.com>
2025-11-26 21:01:35 -0400

    261125-0901: Banco

--- src/Cl_mBanco.ts
+++ src/Cl_mBanco.ts
@@ -1,10 +1,10 @@
 import Cl_mTransaccion, {iTransaccion} from "./Cl_mTransaccion.js";
-
+import Cl_mSalida from "./Cl_mSalida.js";
+import Cl_mEntrada from "./Cl_mEntrada.js";
 export default class Cl_mBanco{
-    private 
-    constructor(){
-
-    }
+    private acmMontoSalida: number = 0;
+    private acmMontoEntrada: number = 0;
+    private cntTransacciones: number = 0;
     private banco: Cl_mTransaccion[] = [];
     agregarTransaccion({
         transaccion,
@@ -37,5 +37,31 @@ export default class Cl_mBanco{
             lista.push(transaccion.toJSON());
         });
         return lista;
+    }
+    procesarTransacciones(t:Cl_mTransaccion) {
+    this.cntTransacciones++
+    if (t instanceof Cl_mSalida) { 
+        this.acmMontoSalida += t.montoF();    
+        }
+    if (t instanceof Cl_mEntrada) { 
+        this.acmMontoEntrada += t.montoF();    
+        }
+    }
+    //Metodos de retorno
+    public montoTotalTSalidas(): number {
+        return this.acmMontoSalida;
+    }
+    public montoTotalTEntradas(): number {
+        return this.acmMontoEntrada;
+    }  
+    public cantidadTransacciones(): number {
+        return this.cntTransacciones;
+    }
+    public resultadoFinanciero(): number | string {
+        if (this.montoTotalTEntradas() > this.montoTotalTSalidas()) {
+            return "Utilidad de" + (this.montoTotalTEntradas() - this.montoTotalTSalidas());
+        } else {
+            return "Perdida de" + (this.montoTotalTSalidas() - this.montoTotalTEntradas());
+        }
     }
 }
